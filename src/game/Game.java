67558b2cb9bff9fabd926d56d38afcc4494690a1
runrey <ernur.bolatkanov@yandex.ru>
2020-06-12 18:10:46 +0600

    Game class

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,8 +1,11 @@
 package game;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class Game {
 	private static Game instance;
-	
+	private static Database link;
 	private Game() {
 		
 	}
@@ -14,7 +17,37 @@ public class Game {
 		return instance;
 	}
 	
-	public static void start(Player player) {
+	public static void start(Player player) throws ClassNotFoundException, SQLException {
+		
+		
 		System.out.println("Game is on developing");
+	}
+	
+
+	
+	public void newRecord(int score, int id) throws ClassNotFoundException, SQLException {
+		this.connection();
+		Game.insertScore(score, id);
+	}
+	
+	public void connection() throws ClassNotFoundException {
+		try {
+			setLink(new Database("localhost", "root", "", "yernur"));
+		}
+		catch(SQLException e) {
+			System.out.println("Connection error: "+e.getMessage());
+		}
+	}
+	
+	public static void insertScore(int score, int id) throws SQLException {
+		link.insert("INSERT INTO score(user_id, score) VALUES ('"+id+"', '"+score+"')");
+	}
+
+	public Database getLink() {
+		return link;
+	}
+
+	private void setLink(Database link) {
+		Game.link = link;
 	}
 }
